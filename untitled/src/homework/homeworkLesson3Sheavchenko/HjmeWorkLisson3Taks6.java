597e84849af27d47049618c844841ad503d42681
andy1 <Andy1991pro@gmail.com>
2022-08-07 14:30:47 +0300

    Home_Work_Lisson3

---
+++ untitled/src/homework/homeworkLesson3Sheavchenko/HjmeWorkLisson3Taks6.java
@@ -0,0 +1,11 @@
+package homework.homeworkLesson3Sheavchenko;
+
+public class HjmeWorkLisson3Taks6 {
+    public static void main(String[] args) {
+        int x=1;
+        for(x=10;x<20;x+=1) {
+            double i= Math.sqrt(x);
+            System.out.println(i);}}}
+
+
+
---
+++ untitled/src/homework/homeworkLesson3Sheavchenko/HomeWorkLisson3TableMultiplication.java
@@ -0,0 +1,9 @@
+package homework.homeworkLesson3Sheavchenko;
+
+public class HomeWorkLisson3TableMultiplication {
+    public static void main(String[] args) {
+        for( int x=0;x<=10;x++)
+        {for(int y =0;y<=x;y++)
+        {int summa = 0;
+            summa += x*y;
+            System.out.println(x+"*"+y+"="+summa);}}}}
