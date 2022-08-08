1816c623a040a31c60553ebcd5df3d742950d144
andy1 <Andy1991pro@gmail.com>
2022-08-08 18:44:11 +0300

    Home_Work_Lisson3

---
+++ untitled/src/homework/homeworkLesson3Sheavchenko/HomeWorkLisson3Subsequence.java
@@ -0,0 +1,7 @@
+package homework.homeworkLesson3Sheavchenko;
+
+public class HomeWorkLisson3Subsequence {
+    public static void main(String[] args) {
+        for(int x=0;x<10;x++){
+            if(x%2==1){System.out.print(x*-5+",");}
+            else {System.out.print(x*5+",");}}}}
