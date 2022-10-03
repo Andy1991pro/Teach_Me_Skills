81575eacee2b86af705b81fc4e461499116c3f4f
andy1 <Andy1991pro@gmail.com>
2022-10-03 18:36:28 +0300

    Home_Work_Lesson3

--- untitled/src/homework/by/homework17/FailReader.java
+++ untitled/src/homework/by/homework17/FailReader.java
@@ -14,10 +14,10 @@ public class FailReader {
     }
     public static void SortList (String file) {
         Map <String, List<String>> allList = new HashMap<>();
-        List<String> listDocument = new ArrayList<>();
-        List <String> validFile = new ArrayList<>();
-        List <String> numberPhone= new ArrayList<>();
-        List <String> email = new ArrayList<>();
+        List<String> listDocuments = new ArrayList<>();
+        List <String> validFiles = new ArrayList<>();
+        List <String> numberPhones= new ArrayList<>();
+        List <String> emails = new ArrayList<>();
         try {
             File file1 = new File(file);
             FileReader path = new FileReader(file1);
@@ -25,29 +25,29 @@ public class FailReader {
 
             while (scanner.hasNextLine()){
                 String document = scanner.nextLine();
-                listDocument.add(document);
+                listDocuments.add(document);
             }
             Pattern pattern = Pattern.compile("\\d{4}.\\w{3}.\\d{4}.\\w{3}.\\d\\w\\d\\w");
-            for (String current :listDocument){
+            for (String current :listDocuments){
                 Matcher matcher = pattern.matcher(current);
                 if (matcher.find()){
-                    validFile.add(matcher.group());}
+                    validFiles.add(matcher.group());}
             }
             Pattern pattern1 = Pattern.compile("\\+\\(\\d{2}\\)\\d{7}$");
-            for (String current :listDocument){
+            for (String current :listDocuments){
                 Matcher matcher = pattern1.matcher(current);
                 if (matcher.find()){
-                    numberPhone.add(matcher.group());}
+                    numberPhones.add(matcher.group());}
             }
             Pattern pattern2 = Pattern.compile("^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$");
-            for (String current :listDocument){
+            for (String current :listDocuments){
                 Matcher matcher = pattern2.matcher(current);
                 if (matcher.find()){
-                    email.add(matcher.group());}
+                    emails.add(matcher.group());}
             }
-            allList.put("email",email);
-            allList.put("document",validFile);
-            allList.put("numberPhone", numberPhone);
+            allList.put("email",emails);
+            allList.put("document",validFiles);
+            allList.put("numberPhone", numberPhones);
             System.out.println(file1.getName()+allList);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
