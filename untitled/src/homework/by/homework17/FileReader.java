ca11e8b1ef7bc57b002ae19d33e7bfb56c12bb6e
andy1 <Andy1991pro@gmail.com>
2022-10-03 19:03:26 +0300

    Home_Work_Lesson3

--- untitled/src/homework/by/homework17/FileReader.java
+++ untitled/src/homework/by/homework17/FileReader.java
@@ -2,53 +2,52 @@ package homework.by.homework17;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class FailReader {
+public class FileReader {
 
 
-    public FailReader() {
+    public FileReader() {
     }
-    public static void SortList (String file) {
-        Map <String, List<String>> allList = new HashMap<>();
+    public static void sortList (String file) {
+        Map <String, List<String>> allDate = new HashMap<>();
         List<String> listDocuments = new ArrayList<>();
         List <String> validFiles = new ArrayList<>();
         List <String> numberPhones= new ArrayList<>();
         List <String> emails = new ArrayList<>();
         try {
             File file1 = new File(file);
-            FileReader path = new FileReader(file1);
+            java.io.FileReader path = new java.io.FileReader(file1);
             Scanner scanner = new Scanner(path);
 
             while (scanner.hasNextLine()){
                 String document = scanner.nextLine();
                 listDocuments.add(document);
             }
-            Pattern pattern = Pattern.compile("\\d{4}.\\w{3}.\\d{4}.\\w{3}.\\d\\w\\d\\w");
+            Pattern documentPattern = Pattern.compile("\\d{4}.\\w{3}.\\d{4}.\\w{3}.\\d\\w\\d\\w");
             for (String current :listDocuments){
-                Matcher matcher = pattern.matcher(current);
+                Matcher matcher = documentPattern.matcher(current);
                 if (matcher.find()){
                     validFiles.add(matcher.group());}
             }
-            Pattern pattern1 = Pattern.compile("\\+\\(\\d{2}\\)\\d{7}$");
+            Pattern phonePattern = Pattern.compile("\\+\\(\\d{2}\\)\\d{7}$");
             for (String current :listDocuments){
-                Matcher matcher = pattern1.matcher(current);
+                Matcher matcher = phonePattern.matcher(current);
                 if (matcher.find()){
                     numberPhones.add(matcher.group());}
             }
-            Pattern pattern2 = Pattern.compile("^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$");
+            Pattern emailPattern = Pattern.compile("^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$");
             for (String current :listDocuments){
-                Matcher matcher = pattern2.matcher(current);
+                Matcher matcher = emailPattern.matcher(current);
                 if (matcher.find()){
                     emails.add(matcher.group());}
             }
-            allList.put("email",emails);
-            allList.put("document",validFiles);
-            allList.put("numberPhone", numberPhones);
-            System.out.println(file1.getName()+allList);
+            allDate.put("email",emails);
+            allDate.put("document",validFiles);
+            allDate.put("numberPhone", numberPhones);
+            System.out.println(file1.getName()+allDate);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
--- untitled/src/homework/by/homework17/Main.java
+++ untitled/src/homework/by/homework17/Main.java
@@ -3,12 +3,12 @@ package homework.by.homework17;
 
 import java.util.Scanner;
 // D:\\Teach_Me_Skills\\my_file.txt Путь для быстрой проверки
-public class Main extends FailReader{
+public class Main extends FileReader {
     public static void main(String[] args) {
         System.out.println("Enter path: ");
         Scanner path1 = new Scanner(System.in);
         String path = path1.nextLine();
-        SortList(path);
+        sortList(path);
 
     }
 }
